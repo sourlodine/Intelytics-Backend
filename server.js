c3577e9955fb8de7c5d6fb1ccd0f17925880040b
sourlodine <harran39318@gmail.com>
2024-04-25 18:19:11 +0900

    fix: add local origin for cors

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
-require('dotenv').config();
-const connectDB = require('./db');
-const {getTokenData,getCurrentPrice,getDataByInterval} = require('./routes');
-const cors = require('cors');
+require("dotenv").config();
+const connectDB = require("./db");
+const {
+  getTokenData,
+  getCurrentPrice,
+  getDataByInterval,
+} = require("./routes");
+const cors = require("cors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(cors({
-    origin: 'https://www.intelytics.net',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}));
+app.use(
+  cors({
+    origin: ["https://www.intelytics.net", "http://localhost:3000"],
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
 
 // Connect to the database
 connectDB();
 
-const PORT= process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// enable json parsing 
+// enable json parsing
 app.use(express.json());
-app.use('/api', getTokenData);
-app.use('/api', getCurrentPrice);
-app.use('/api', getDataByInterval);
+app.use("/api", getTokenData);
+app.use("/api", getCurrentPrice);
+app.use("/api", getDataByInterval);
 
-app.use('/', (req, res) => {
-    res.send('Welcome to the Intelytics API');
+app.use("/", (req, res) => {
+  res.send("Welcome to the Intelytics API");
 });
 
 app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
 
 // for vercel
